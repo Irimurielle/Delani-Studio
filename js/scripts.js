97bda681ce78a99f8be950f00ba0d7fab7b458a3
Irimurielle <irimurielle@gmail.com>
2020-10-05 07:01:49 +0200

    added speed for toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,38 @@
 $(document).ready(function() {
     $('.design').click(function() {
-        $('.design').toggle();
-        $('.des').toggle();
+        $('.design').toggle('slow');
+        $('.des').toggle('slow');
     });
     $('.des').click(function () {
-        $('.design').toggle();
-        $('.des').toggle();
+        $('.design').toggle('slow');
+        $('.des').toggle('slow');
     });
     $('.develop').click(function() {
-        $('.develop').toggle();
-        $('.devl').toggle();
+        $('.develop').toggle('slow');
+        $('.devl').toggle('slow');
     });
     $('.devl').click(function () {
-        $('.develop').toggle();
-        $('.devl').toggle();
+        $('.develop').toggle('slow');
+        $('.devl').toggle('slow');
     });
     $('.product').click(function() {
-        $('.product').toggle();
-        $('.prod').toggle();
+        $('.product').toggle('slow');
+        $('.prod').toggle('slow');
     });
     $('.prod').click(function () {
-        $('.product').toggle();
-        $('.prod').toggle();
+        $('.product').toggle('slow');
+        $('.prod').toggle('slow');
     });
     $('.imageport').hover(function(){
-        $('.portinfo',this).slideToggle();
+        $('.portinfo',this).slideToggle('slow');
      }, function(){
-        $('.port-info',this).slideToggle();
+        $('.port-info',this).slideToggle('slow');
      });
     $("#myForm").submit(function(){
         event.preventDefault();
         var name = $("input#name").val();
         var email = $("input#email").val();
         var message = $("textarea#message").val();
-        alert("Thank you for reaching out to us, we will soon get back to you");
+        alert(name + " " + "thank you for reaching out to us, we will soon get back to you");
     });
 });
